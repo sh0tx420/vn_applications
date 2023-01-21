237781d7affc9316942dbcc48e1d43de9c7da249
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2023-01-22 01:14:13 +0200

    did stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import VHeader from "./components/vheader";
 import VText from "./components/vtext";
 import {
-    VStack, HStack, Divider, FormControl, FormHelperText, Input, SimpleSelect, SimpleOption, Radio, RadioGroup, Textarea, Button
+    VStack, HStack, Divider, FormControl, FormHelperText, Input, SimpleSelect, SimpleOption, Radio, RadioGroup, Textarea, Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalCloseButton,
+    ModalHeader,
+    ModalBody,
+    createDisclosure
 } from "@hope-ui/solid";
-import { For, createSignal } from "solid-js";
+import { For, Show, createSignal } from "solid-js";
 import { timezones_gmt } from "./data";
+import HCaptcha from "solid-hcaptcha";
 
 
 function App() {
+    let hcaptcha;
 
     const [form, setForm] = createSignal({
         server: "VoidRP",
@@ -23,8 +32,31 @@ function App() {
     // shit hack because fuck solidjs
     const [timezone, setTimezone] = createSignal("");
 
+    // j
+    const [loading, setLoading] = createSignal(false);
+    const [captchaRes, setCaptchaRes] = createSignal(null);
+
+    const { isOpen, onOpen, onClose } = createDisclosure()
+
+    const submitCaptcha = async () => {
+        if (!hcaptcha) return;
+        const response = await hcaptcha.execute();
+        setCaptchaRes(response);
+    };
+
+    // regex hub
+    function isInvalid(formtype) {
+        switch (formtype) {
+            case "discord":
+                if (!form()["discord"].match(/[a-zA-Z]#[0-9]{4}$/)) return true;
+            case "steam":
+                if (!form()["steamid"].match(/^STEAM_[0|1]:[0|1]:[0-9]{9}$/)) return true;
+        }
+    }
+
     return (
         <div class="flex justify-center content-center">
+            <HCaptcha sitekey={`${import.meta.env.HCAPTCHA_SITEKEY}`} onLoad={hcaptcha_instance => (hcaptcha = hcaptcha_instance)} size="invisible" />
             <div class="mt-8 mb-8 p-2 bg-neutral-800 rounded-lg w-2/5">
                 <VHeader>Staff Application</VHeader>
                 <Divider />
@@ -36,14 +68,25 @@ function App() {
                         </SimpleSelect>
                     </FormControl>
 
-                    <FormControl>
+                    <FormControl invalid={isInvalid("discord")}>
                         <VText>What is your Discord name and tag?</VText>
                         <Input placeholder="Username#0000" size="lg" value={form()["discord"]} onInput={(e) => setForm(dict => ({...dict, discord: e.currentTarget.value}))}/>
+                        <Show when={isInvalid("discord")}>
+                            <FormHelperText>
+                                Invalid Discord username.
+                            </FormHelperText>
+                        </Show>
                     </FormControl>
 
-                    <FormControl>
+                    <FormControl invalid={isInvalid("steam")}>
                         <VText>What is your Steam ID?</VText>
                         <Input placeholder="STEAM_0:0:000000000" size="lg" value={form()["steamid"]} onInput={(e) => setForm(dict => ({...dict, steamid: e.currentTarget.value}))}/>
+                        <Show when={isInvalid("steam")}>
+                            <FormHelperText>
+                                Invalid SteamID.
+                            </FormHelperText>
+                        </Show>
+                        <br />
                         <FormHelperText>
                             <VText>
                                 You can grab your Steam ID from <a href="https://steamid.io/" class="text-red-600">here</a>.
@@ -61,31 +104,6 @@ function App() {
                             </HStack>
                         </RadioGroup>
                     </FormControl>
-
-                    {/*
-                    <FormControl>
-                        <VText>What is your timezone?</VText>
-                        <Select value={form()["timezone"]} onChange={(e) => setForm(dict => ({...dict, timezone: e.currentTarget.value}))}>
-                            <SelectTrigger>
-                                <SelectPlaceholder>GMT 00:00</SelectPlaceholder>
-                                <SelectValue />
-                                <SelectIcon />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectListbox>
-                                    <For each={timezones_gmt}>
-                                        {tz => (
-                                            <SelectOption value={tz}>
-                                                <SelectOptionText>GMT {tz}</SelectOptionText>
-                                                <SelectOptionIndicator />
-                                            </SelectOption>
-                                        )}
-                                    </For>
-                                </SelectListbox>
-                            </SelectContent>
-                        </Select>
-                    </FormControl>
-                    */}
 
                     <FormControl>
                         <VText>What is your timezone?</VText>
@@ -117,15 +135,45 @@ function App() {
 
                 </VStack>
 
-                <Button class="m-4 bg-red-700 hover:bg-red-500" onClick={
-                    () => fetch("localhost:3100/discord", {
-                        method: "POST",
-                        body: `${form()}${timezone}`
-                    })
+                <Button class="m-4 bg-red-700 hover:bg-red-500" loading={loading()} onClick={
+                    () => {
+                        submitCaptcha;
+
+                        setForm(dict => ({...dict, timezone: timezone()}));
+                        setLoading(true);
+                        fetch("https://api.voidnetworks.org/discord", {
+                            method: "POST",
+                            mode: "no-cors",
+                            cache: "no-cache",
+                            referrerPolicy: "origin",
+                            body: `${JSON.stringify(form())}`,
+                        }).then(() => {
+                            setLoading(false);
+                            onOpen(true);
+                            setTimeout(() => {
+                                window.location.reload()
+                            }, 1500);
+                        })
+                    }
                 }>
                     Submit
                 </Button>
             </div>
+
+            <Modal opened={isOpen()} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalCloseButton />
+                        <ModalHeader>
+                            <VHeader>
+                                Request Success
+                            </VHeader>
+                        </ModalHeader>
+                        <ModalBody>
+                            yea
+                        </ModalBody>
+                </ModalContent>
+            </Modal>
         </div>
     );
 }
